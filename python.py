61cd1270b673967485d931b683dccff51a804325
aruzhan0606 <165879261+aruzhan0606@users.noreply.github.com>
2024-04-23 13:00:04 +0500

    Update python.py

--- python.py
+++ python.py
@@ -29,3 +29,12 @@ def check_numbers(x,y):
         return("кіші")
 print(check_numbers(40,50))
 
+def find_max(list1):
+    max_number = list1[0]
+    for a in list1:
+        if a > max_number:
+            max_number = a
+    return max_number
+
+print("en ulken san",find_max([1, 2, 3, 4, 5]))
+
